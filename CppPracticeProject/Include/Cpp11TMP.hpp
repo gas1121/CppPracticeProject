2397751fe2d7d6b6bee9bd6b29328a52e6d13f47
gas1121 <jtdzhx@gmail.com>
2016-11-16 17:46:06 +0800

    Update TMP learning case.

--- CppPracticeProject/Include/Cpp11TMP.hpp
+++ CppPracticeProject/Include/Cpp11TMP.hpp
@@ -4,6 +4,8 @@
 #include <tuple>
 
 namespace CppPractice {
+	//from http://pdimov.com/cpp2/simple_cxx11_metaprogramming.html
+
 	//alias templates
 	template<typename U>
 	struct remove_pointer
@@ -37,6 +39,27 @@ namespace CppPractice {
     template<template<typename...> typename F, typename L>
     using mp_apply = mp_rename<L, F>;
 
+	template<typename T>
+	using mp_add_pointer = T*;
+
+	template<typename L, typename... T> struct mp_push_front_impl;
+	template<template<typename...> typename L, typename... U, typename... T>
+	struct mp_push_front_impl<L<U...>, T...>
+	{
+		using type = L<T..., U...>;
+	};
+	template<typename L, typename... T>
+	using mp_push_front = typename mp_push_front_impl<L, T...>::type;
+
+	template<template<typename...> typename F, typename L> struct mp_transform_impl;
+	template<template<typename...> typename F, template<typename...> typename L, typename... T>
+	struct mp_transform_impl<F, L<T...>>
+	{
+		using type = L<F<T>...>;
+	};
+	template<template<typename...> typename F, typename L>
+	using mp_transform = typename mp_transform_impl<F, L>::type;
+
 	void Cpp11TMPMain()
 	{
 		//static assert
@@ -47,5 +70,10 @@ namespace CppPractice {
 		static_assert(std::is_same<std::tuple<int,float>, decltype(testTuple)>::value, "change failed.");
 
 		static_assert(mp_size<mp_list<int, float, char>>::value == 3, "type count error.");
+
+		using input = std::tuple<int, void, float>;
+		using expected = std::tuple<int*, void*, float*>;
+		using result = mp_transform<mp_add_pointer, input>;
+		static_assert(std::is_same<result, expected>::value, "");
 	}
 }
