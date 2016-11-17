d9ece5cfd06e23c7cd668731cb3a53532e410fa5
gas1121 <jtdzhx@gmail.com>
2016-11-17 18:28:05 +0800

    Update TMP learning case.

--- CppPracticeProject/Include/Cpp11TMP.hpp
+++ CppPracticeProject/Include/Cpp11TMP.hpp
@@ -51,14 +51,23 @@ namespace CppPractice {
 	template<typename L, typename... T>
 	using mp_push_front = typename mp_push_front_impl<L, T...>::type;
 
-	template<template<typename...> typename F, typename L> struct mp_transform_impl;
+	template<template<typename...> typename F, typename... L> struct mp_transform_impl;
+	template<template<typename...> typename F, typename... L>
+	using mp_transform = typename mp_transform_impl<F, L...>::type;
 	template<template<typename...> typename F, template<typename...> typename L, typename... T>
 	struct mp_transform_impl<F, L<T...>>
 	{
 		using type = L<F<T>...>;
 	};
-	template<template<typename...> typename F, typename L>
-	using mp_transform = typename mp_transform_impl<F, L>::type;
+	template<template<typename...> typename F, 
+		template<typename...> typename L1, typename... T1,
+		template<typename...> typename L2, typename... T2>
+	struct mp_transform_impl<F, L1<T1...>, L2<T2...>>
+	{
+		static_assert(sizeof...(T1) == sizeof...(T2),
+			"The arguments of mp_transform2 should be of the same size");
+		using type = L1<F<T1, T2>...>;
+	};
 
 	void Cpp11TMPMain()
 	{
@@ -75,5 +84,10 @@ namespace CppPractice {
 		using expected = std::tuple<int*, void*, float*>;
 		using result = mp_transform<mp_add_pointer, input>;
 		static_assert(std::is_same<result, expected>::value, "");
+
+		using input2 = std::pair<std::tuple<int, double, float>, std::tuple<double, char, long>>;
+		using expected2 = std::tuple<std::pair<int, double>, std::pair<double, char>, std::pair<float, long>>;
+		using result2 = mp_transform < std::pair, input2::first_type, input2::second_type>;
+		static_assert(std::is_same<result2, expected2>::value, "");
 	}
 }
